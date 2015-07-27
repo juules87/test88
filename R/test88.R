4e0965a60f50658d8bae669b59a6e97d6a94ae24
unknown <jhbreincke@gmail.com>
2015-07-27 21:17:39 +0200

    ok

--- R/test88.R
+++ R/test88.R
@@ -1,8 +1,3 @@
-test88<-function(x){
-  library(RMySQL) 
-  con<-dbConnect(MySQL(),user='deata',password='Welkom22',dbname='deata_data',host='149.210.144.46',port=3306)
-  query<-paste("select max(Visitors) from deata_data.data where Userid='",x,"'",sep="" )
-  result<-dbSendQuery(con,query)
-  y<-fetch(result)[1,1]
-  y
+test<-function(x){
+sqrt(x)
 }
